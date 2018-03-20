38b5361b738598e8efff00b9070c8e2dd98603cf
2ndPerk <tymonek.wl@gmail.com>
2018-03-19 22:39:37 -0300

    Initial Commit added src folder

--- TokenizedException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Riley Newson on 18-03-06.
- */
-public class TokenizedException extends Exception{
-    public TokenizedException(String message){
-        super(message);
-    }
-}
