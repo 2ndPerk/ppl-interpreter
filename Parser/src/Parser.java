a0edeb8ddd9d24b083a7484556091aa0572b2411
2ndPerk <tymonek.wl@gmail.com>
2018-03-21 16:13:24 -0300

    Works Does not output "end"

--- Parser/src/Parser.java
+++ Parser/src/Parser.java
@@ -8,9 +8,9 @@ public class Parser {
     public static void main(String[] args) {
         List<Token> tokens = Scanner.getTokens(args);
 
-        /*for( int i = 0; i < tokens.size(); i++){
+        for( int i = 0; i < tokens.size(); i++){
             System.out.println(tokens.get(i).toString());
-        }*/
+        }
 
         TokenParse parser = new TokenParse(tokens);
         System.out.println(parser.parseProgram(0));
--- Parser/src/TokenParse.java
+++ Parser/src/TokenParse.java
@@ -13,9 +13,11 @@ public class TokenParse {
 
 
     public String parseProgram(int ind){
+        System.out.println("Parsing Start");
         try{
             String retString = "Program \n";
             while(++curr < tokens.size()) {
+                //System.out.println(tokens.get(curr).toString());
                 retString += parseTopDef(ind + 1);
             }
 
@@ -27,11 +29,11 @@ public class TokenParse {
     }
 
     private String parseTopDef(int ind) throws TokenizedException{
-
+        System.out.println("TopDef");
         Token c = tokens.get(curr);
         String ret =  "TopDef \n";
 
-        System.out.println(c.getIntType());
+        //System.out.println(c.getIntType());
 
         if(c.getIntType() == 5){
             ret = ret + this.parseFunDef(ind + 1);
@@ -40,7 +42,7 @@ public class TokenParse {
         }else if(c.getIntType() == 7){
             ret = ret + this.parseTypeDef(ind + 1);
         }else{
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(TopDef) Error for token with value: "+ c.toString()
                                             + ", at Line: " + c.getLineCount()
                                             + ", Word: " + c.getWordCount());
         }
@@ -50,37 +52,43 @@ public class TokenParse {
         return ret;
     }
     private String parseFunDef(int ind) throws TokenizedException{
+        System.out.println("FunDef");
         Token c = tokens.get(curr);
         String ret = c.toString() + "\n";
         ret = indent(ind + 1) + ret;
         c = tokens.get(++curr);
+        //System.out.println(curr);
         if(c.getIntType() != 9){
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(FunDef) Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }else{
             ret = ret +indent(ind + 1)+c.toString()+ "\n";
         }
         int t = tokens.get(++curr).getIntType();
+        //System.out.println(curr);
         if(t < 13 || t == 14) {
             ret = ret + this.parseBody(ind + 1);
         }
-        c = tokens.get(++curr);
+        c = tokens.get(curr);
+        //System.out.println(curr);
         if(c.getIntType() != 16){
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(FunDef 2)Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }
         ret = indent(ind) + "FunDef\n" + ret;
         return ret;
     }
-    private String parseCofunDef(int ind){
+    private String parseCofunDef(int ind) throws TokenizedException {
+        System.out.println("Cofundef");
         Token c = tokens.get(curr);
         String ret = c.toString() + "\n";
         ret = indent(ind) + ret;
         c = tokens.get(++curr);
+        //System.out.println(curr);
         if(c.getIntType() != 9){
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(Cofundef) Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }else{
@@ -88,44 +96,52 @@ public class TokenParse {
             ret = ret +indent(ind + 1)+ c.toString()+"\n";
         }
         int t = tokens.get(++curr).getIntType();
+        //System.out.println(curr);
         if(t < 13 || t == 14) {
             ret = ret + this.parseBody(ind + 1);
         }
-        c = tokens.get(++curr);
+        c = tokens.get(curr);
+        //System.out.println(curr);
         if(c.getIntType() != 16){
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(Cofundef 2)Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }
         ret = indent(ind) + "CofunDef\n" + ret;
         return ret;
     }
-    private String parseTypeDef(int ind){
+    private String parseTypeDef(int ind) throws TokenizedException{
+        System.out.println("typedef");
         //type
         Token c = tokens.get(curr);
         String ret = indent(ind) + c.toString() + "\n";
         //id
         c = tokens.get(++curr);
+        //System.out.println(curr);
         if(c.getIntType() == 9){
             ret = ret + indent(ind + 1) + c.toString() + "\n";
         }else{
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(TypeDef) Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }
         //Fields
         if(tokens.get(++curr).getIntType() == 9){
+            //System.out.println(curr);
             ret = ret + parseFields(ind);
         }
+        //System.out.println(curr);
         return ret;
     }
-    private String parseFields(int ind){
+    private String parseFields(int ind) throws TokenizedException{
+        System.out.println("fields");
         String ret = "";
         Token c = tokens.get(curr);
+        //System.out.println(curr);
         if(c.getIntType() == 9){
             ret = ret + indent(ind + 1) + c.toString() + "\n";
         }else{
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(Fields) Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }
@@ -135,22 +151,25 @@ public class TokenParse {
         }
         return ret;
     }
-    private String parseBody(int ind){
+    private String parseBody(int ind) throws TokenizedException{
+        System.out.println("body");
         String ret = "";
         ret = ret + this.parseStatement(ind + 1);
         int t = tokens.get(++curr).getIntType();
-        System.out.println(t + " Body");
+        //System.out.println(curr);
+        //System.out.println(t + " Body");
         if(t < 13 || t == 14) {
             ret = ret + this.parseBody(ind + 1);
         }
         ret = indent(ind) + "Body\n" + ret;
         return ret;
     }
-    private String parseStatement(int ind){
+    private String parseStatement(int ind) throws TokenizedException{
+        System.out.println("statement");
         String ret = "";
         Token c = tokens.get(curr);
         int t = c.getIntType();
-        System.out.println(t + " Statement");
+        // System.out.println(t + " Statement");
         if(t == 5){
             ret = ret + this.parseFunDef(ind + 1);
         }else if(t == 6){
@@ -170,28 +189,32 @@ public class TokenParse {
         ret = temp + ret;
         return ret;
     }
-    private String parseVarDef(int ind){
+    private String parseVarDef(int ind) throws TokenizedException{
+        System.out.println("vardef");
         String ret = indent(ind) + "VarDef\n";
         Token c = tokens.get(curr);
 
         ret = ret + indent(ind + 1) + c.toString() + "\n";
         c = tokens.get(++curr);
+        //System.out.println(curr);
         if(c.getIntType() != 9){
             ret = ret + indent(ind + 1) + c.toString() + "\n";
         }else{
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(VarDef) Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }
         return ret;
     }
-    private String parseSimpleStatement(int ind){
+    private String parseSimpleStatement(int ind) throws TokenizedException{
+        System.out.println("simplestatement");
         Token c = tokens.get(curr);
         String ret = c.toString()+"\n";
         ret = indent(ind) + "SimpleStatement\n" + indent(ind+1) + ret;
         return ret;
     }
-    private String parseLambda(int ind){
+    private String parseLambda(int ind) throws TokenizedException{
+        System.out.println("lambda");
         String ret = indent(ind) + "Lamda\n";
         if(tokens.get(curr).getIntType() == 12){
             ret = ret + parseFunLambda(ind + 1);
@@ -200,38 +223,44 @@ public class TokenParse {
         }
         return ret;
     }
-    private String parseFunLambda(int ind){
+    private String parseFunLambda(int ind) throws TokenizedException{
+        System.out.println("funlambda");
         String ret = indent(ind) + "FunLambda";
         Token c = tokens.get(curr);
 
         ret = ret + indent(ind + 1) + c.toString() + "\n";
 
         c = tokens.get(++curr);
+        //System.out.println(curr);
         ret = ret + parseBody(ind + 1);
 
-        c = tokens.get(++curr);
+        c = tokens.get(curr);
+        //System.out.println(curr);
         if(c.getIntType() != 13){
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(FunLambda) Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }else{
             ret = ret + indent(ind + 1) + c.toString() + "\n";
         }
-
+        //System.out.println("Lambda End");
         return ret;
     }
-    private String parseCofunLambda(int ind){
+    private String parseCofunLambda(int ind) throws TokenizedException{
+        System.out.println("cofunlambda");
         String ret = indent(ind) + "CofunLambda";
         Token c = tokens.get(curr);
 
         ret = ret + indent(ind + 1) + c.toString() + "\n";
 
         ++curr;
+        //System.out.println(curr);
         ret = ret + parseBody(ind + 1);
 
-        c = tokens.get(++curr);
+        c = tokens.get(curr);
+        //System.out.println(curr);
         if(c.getIntType() != 15){
-            throw new TokenizedException("Error for token with value: "+ c.toString()
+            throw new TokenizedException("(CofunLambda) Error for token with value: "+ c.toString()
             + ", at Line: " + c.getLineCount()
             + ", Word: " + c.getWordCount());
         }else{
